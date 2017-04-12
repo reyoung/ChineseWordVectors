c9bd1f267752191f41a556dfe313ebc0e406ddfe
Yu Yang <yuyang18@baidu.com>
2017-04-12 16:59:11 +0800

    Move files

--- preprocess.sh
+++ preprocess.sh
@@ -3,14 +3,14 @@
 cd `dirname $0`
 echo "Generating Word Dict"
 
-pv data/data/* | python generate_word_dict.py > word_dict
+pv data/data/* | python preprocess_scripts/generate_word_dict.py > word_dict
 
 echo "Convert Word Dict to Python Pickle Format"
-pv word_dict | python convert_word_dict_to_pkl.py >word_dict.pkl
+pv word_dict | python preprocess_scripts/convert_word_dict_to_pkl.py >word_dict.pkl
 
 echo "Convert Text to Pickle Format"
 mkdir -p preprocessed/data/data
 
 sz=`find ./data/data/ -name 'data_*'| wc -c`
 
-find ./data/data/ -name 'data_*' | xargs -Iop -P3 python word_to_wordids.py op ./preprocessed/op
+find ./data/data/ -name 'data_*' | xargs -Iop -P3 python preprocess_scripts/word_to_wordids.py op ./preprocessed/op
