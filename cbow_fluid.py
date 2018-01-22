3e13b9c39621d9d342d77d0dc47f38fc7e8abbb7
Yang Yu <yuyang18@baidu.com>
2018-01-22 14:11:45 +0800

    Add Parallel.Do

--- cbow_fluid.py
+++ cbow_fluid.py
@@ -8,7 +8,8 @@ import paddle.v2 as paddle
 MAX_DICT_SIZE = 14898
 
 
-def main(window_size, dict_size=10000, emb_size=32):
+def main(window_size, dict_size=10000, emb_size=32, num_neg_samples=10,
+         batch_size=512):
     assert window_size % 2 == 1
     words = []
     for i in xrange(window_size):
@@ -18,25 +19,33 @@ def main(window_size, dict_size=10000, emb_size=32):
     dict_size = min(MAX_DICT_SIZE, dict_size)
     label_word = int(window_size / 2) + 1
 
-    embs = []
-    for i in xrange(window_size):
-        if i == label_word:
-            continue
+    for_loop = fluid.layers.ParallelDo(fluid.layers.get_places())
+    with for_loop.do():
+        embs = []
+        for i in xrange(window_size):
+            if i == label_word:
+                continue
 
-        emb = fluid.layers.embedding(input=words[i],
-                                     size=[dict_size, emb_size],
-                                     param_attr='emb.w',
-                                     is_sparse=True)
+            emb = fluid.layers.embedding(input=for_loop.read_input(words[i]),
+                                         size=[dict_size, emb_size],
+                                         param_attr='emb.w',
+                                         is_sparse=False)
 
-        embs.append(emb)
+            embs.append(emb)
 
-    embs = fluid.layers.concat(input=embs, axis=1)
-    loss = fluid.layers.nce(input=embs,
-                            label=words[label_word],
-                            num_total_classes=dict_size,
-                            param_attr='nce.w',
-                            bias_attr='nce.b')
-    avg_loss = fluid.layers.mean(x=loss)
+        embs = fluid.layers.concat(input=embs, axis=1)
+        loss = fluid.layers.nce(input=embs,
+                                label=for_loop.read_input(words[label_word]),
+                                num_total_classes=dict_size,
+                                param_attr='nce.w',
+                                bias_attr='nce.b',
+                                num_neg_samples=num_neg_samples)
+        avg_loss = fluid.layers.mean(x=loss)
+        avg_loss /= num_neg_samples + 1
+
+        for_loop.write_output(avg_loss)
+
+    avg_loss = fluid.layers.mean(x=for_loop())
     adam = fluid.optimizer.Adagrad(learning_rate=1e-3)
     adam.minimize(loss=avg_loss)
 
@@ -50,14 +59,14 @@ def main(window_size, dict_size=10000, emb_size=32):
         paddle.reader.buffered(
             reader_creator(window_size=window_size,
                            word_limit=dict_size - 1,
-                           path="./preprocessed"), 4000), 512)
+                           path="./preprocessed"), 4000), batch_size)
 
     for pass_id in xrange(100):
         fluid.io.save_params(exe, dirname='model_{0}'.format(pass_id))
         for batch_id, data in enumerate(reader()):
             avg_loss_np = exe.run(feed=feeder.feed(data), fetch_list=[avg_loss])
-            print "Pass ID {0}, Batch ID {1}, Loss {2}".format(pass_id, batch_id, avg_loss_np)
+            print "Pass ID {0}, Batch ID {1}, Loss {2}".format(pass_id, batch_id, avg_loss_np[0])
 
 
 if __name__ == '__main__':
-    main(window_size=5)
+    main(window_size=5, batch_size=2048)
